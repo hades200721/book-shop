29a06d1ce1e2ea776f2d67e52bbfa633d349f971
Gleb Pendler <glebp@post.bgu.ac.il>
2017-05-13 00:42:24 +0300

    moving foward

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: [ './header.component.css' ]
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent {
 
+    @Output() pageSelected: EventEmitter<number> = new EventEmitter<number>();
+
+    setPage(page: number) {
+        this.pageSelected.emit(page);
+    }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() setRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('cake', 'whatever', 'http://images.media-allrecipes.com/userphotos/600x600/4504513.jpg'),
-    new Recipe('A Test Recipe', 'whatever', 'http://images.media-allrecipes.com/userphotos/600x600/4358589.jpg')
-    ];
+    new Recipe('A Test Recipe', 'descreption goes here...', 'http://images.media-allrecipes.com/userphotos/600x600/4358589.jpg'),
+    new Recipe('pinapplepie', 'pen apple and pinapple', 'https://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/c0.135.1080.1080/12531079_1225886307426286_774845945_n.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelect(recipe: Recipe) {
+    this.setRecipe.emit(recipe);
+  }
 }
